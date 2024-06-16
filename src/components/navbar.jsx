5141e9d4a5a8149c77aa19ebb9d94004a742b62d
jodinotnice <jordan.akpovi@gmail.com>
2024-06-17 00:53:07 +0200

    Correction oublie redirection navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -54,12 +54,12 @@ export default function Navbar() {
               </a>
             </li>
             <li>
-              <a href="#projet" onClick={toggleMenu}>
+              <a href="#competences" onClick={toggleMenu}>
                 Compétences
               </a>
             </li>
             <li>
-              <a href="#competences" onClick={toggleMenu}>
+              <a href="#projet" onClick={toggleMenu}>
                 Projets
               </a>
             </li>
